5aa6eae30efde933a8cfd02bb6c7434c4b08c59d
Abhinav Gautam <abhinav.nanhey@gmail.com>
2018-06-29 20:19:07 +0530

    changes

--- js/cli.js
+++ js/cli.js
@@ -15,7 +15,7 @@ const struct = {
 
 const commands = {}
 let systemData = {}
-const rootPath = 'users/codebytere/root'
+const rootPath = 'users/Gotham13121997/root'
 
 const getDirectory = () => localStorage.directory
 const setDirectory = (dir) => { localStorage.directory = dir }
